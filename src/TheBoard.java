d03d3c31ad3e8526960657caa098036028e3ff38
hayuan98 <hayuan98@seas.upenn.edu>
2025-10-27 19:17:01 -0400

    Revised TheBoard class

--- src/TheBoard.java
+++ src/TheBoard.java
@@ -4,15 +4,16 @@ public class TheBoard {
     private String[] [] TheBoard;
 
     public TheBoard () {
-        TheBoard = new String[4][5];
+        TheBoard = new String[5][5];
 
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 5; j++) {
-                TheBoard[i][j] = " " + Integer.toString(i * 5 + j + 1) + " ";
+                TheBoard[i][j] = " 0" + Integer.toString(i * 5 + j + 1) + " ";
+                TheBoard [1][4] = " 10 ";
             }
         }
         for (int i = 2; i < 3; i++) {
-            for (int j = 1; j <4; j++) {
+            for (int j = 1; j < 4; j++) {
                 TheBoard[i][j] = " " + Integer.toString(i * 5 + j) + " ";
             }
         }
@@ -21,33 +22,103 @@ public class TheBoard {
                 TheBoard[i][j] = " " + Integer.toString(i * 5 + j - 2) + " ";
             }
         }
-    }
-
-    public String getValue (int i, int j) {
-        return TheBoard [i][j];
-    }
-
-    public void placeCard (int location, Card currentCard) {
-        int i;
-        int j;
-        if (location <= 10) {
-           i = location / 5;
-           j = location % 5 - 1;
-        } else if (location <= 13){
-            i = location / 10 + 1;
-            j = location % 10;
-        } else {
-            i = location / 10 + 2;
-            j = location % 10 - 3;
+        for (int i =4; i < 5; i++) {
+            TheBoard[i][0] = "Discarded:";
+            for (int j = 1; j < 5; j++){
+                TheBoard[i][j] = " " + Integer.toString(i * 4 + j)+ " ";
+            }
         }
+
+        TheBoard[2][0] = "    ";
+        TheBoard[3][0] = "    ";
+        TheBoard[2][4] = "    ";
+        TheBoard[3][4] = "    ";
+    }
+
+    public String getValue (int location) {
+        int a;
+        int b;
+        if (location < 0 || location > 20) {
+            String invalidInputMessage= "Invalid spot. If you would like to discard the card, please enter a number between 17-20; otherwise please enter a number between 1-16.";
+            return invalidInputMessage;
+        } else if (location <= 16) {
+            if (location <= 10) {
+                a = location / 6;
+                b = location % 6 - 1;
+            } else if (location <= 13) {
+                a = location / 10 + 1;
+                b = location % 10;
+            } else {
+                a = location / 10 + 2;
+                b = location % 10 - 3;
+            }
+            return TheBoard[a][b];
+        } else {
+            int discardCount = 0;
+            for (int i = 1; i < 5; i++){
+                String currentValue = TheBoard[4][i];
+                String originalValue = " " + Integer.toString(4 * 4 + i) + " ";
+                if (!currentValue.equals(originalValue)){
+                    discardCount++;
+                }
+            }
+            int discardsRemaining = 4 - discardCount;
+            return "Discards remaining: " + Integer.toString(discardsRemaining) +".";
+        }
+    }
+
+    public void placeCard ( int currentLoc, Card currentCard){
+        int i = 0;
+        int j = 0;
         String cardNumSuit = " " + currentCard.getNumOfCard() + currentCard.getSuitOfCard() + " ";
-        String originalValue = " " + Integer.toString(location) + " ";
-        String currentValue = TheBoard [i][j];
+        String originalValue;
+        if (currentLoc<10) {
+            originalValue = " 0" + Integer.toString(currentLoc) + " ";
+        } else {
+            originalValue = " " + Integer.toString(currentLoc) + " ";
+        }
+        if (currentLoc < 0 || currentLoc > 20) {
+            System.out.println("Invalid spot. If you would like to discard the card, please enter a number between 17-20; otherwise please enter a number between 1-16.");
+        } else if (currentLoc <= 16) {
+            if (currentLoc <= 10) {
+                i = currentLoc / 6;
+                j = currentLoc % 6 - 1;
+            } else if (currentLoc <= 13) {
+                i = currentLoc / 10 + 1;
+                j = currentLoc % 10;
+            } else {
+                i = currentLoc / 10 + 2;
+                j = currentLoc % 10 - 3;
+            }
+        } else {
+            if (currentLoc == 17){
+                i = 4;
+                j = 1;
+            } else if (currentLoc == 18) {
+                i = 4;
+                j = 2;
+            } else if (currentLoc == 19) {
+                i = 4;
+                j = 3;
+            } else {
+                i = 4;
+                j = 4;
+            }
+        }
+        String currentValue = TheBoard[i][j];
         if (currentValue.equals(originalValue)) {
             TheBoard[i][j] = cardNumSuit;
         } else {
             System.out.println("Unable to place two cards in the same spot. Please choose another spot.");
         }
+    }
 
+    public void printTheBoard () {
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                System.out.print(TheBoard[i][j]);
+            }
+            System.out.println(TheBoard[i][4]);
+        }
     }
 }
