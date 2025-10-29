6cff079072a64aec8c8ff8b21a2bd1c261d085a7
hayuan98 <hayuan98@seas.upenn.edu>
2025-10-29 01:08:23 -0400

    updated the Score class to use arraylist instead of arrays to avoid repetitive array set ups for hand 1 - hand 9. Relinked the BlackjackSolitaire class to the updated Score class.

--- src/Card.java
+++ src/Card.java
@@ -6,8 +6,6 @@ public class Card {
     private int cardValueMax; // indicate maximum value of this card
     private int cardValueMin; // indicate minimum value of this card
     private int indexOfCard; // each card is represented by a number between 1-52;
-    private int xOfCard; // the x coordinate of this card on the board
-    private int yOfCard; // the y coordinate of this card on the board
     private String cardNumSuit;
 
     // constructor:
@@ -78,39 +76,8 @@ public class Card {
         return indexOfCard;
     }
 
-    public int getxOfCard () {
-        return xOfCard;
-    }
-
-    public int getyOfCard () {
-        return yOfCard;
-    }
 
 
 
 
-
-    // to determine card location on the board:
-    public void cardLocation (int spotNum){
-        int remainder = spotNum % 5;
-        yOfCard = spotNum / 5;
-        if (spotNum < 17) {
-            if (spotNum <= 10){
-                if (remainder != 0){
-                    xOfCard = remainder - 1;
-                } else {
-                    xOfCard = yOfCard * 5 - 1;
-                }
-            } else {
-                int newRemainder = spotNum - 10;
-                if (newRemainder % 3 != 0){
-                    xOfCard = newRemainder;
-                } else {
-                    xOfCard = 3;
-                }
-            }
-            System.out.println("The card is placed on" + spotNum + "at (" + xOfCard +", " + yOfCard + ").");
-        }
-        // if spotNum is >= 17, the card will be placed in discard pile and won't be assigned a location on the board.
-    }
 }
